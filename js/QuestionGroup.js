26b2201138885b8b02a433ea6051d797adfcf2d3
Matt Bocek <mbocek@gmail.com>
2013-08-13 17:17:55 -0700

    Questions appear by group
    questions appear onscreen in groups instead of singles

--- js/QuestionGroup.js
+++ js/QuestionGroup.js
@@ -1,12 +1,28 @@
-function QuestionGroup(param,label){
-	this.meta = param.meta;
-	this.label = param.label;
+function QuestionGroup(param){   
+    this.label = param.label;
+    this.meta = param.meta || '';
+    this.questions = [];
+    this.cssClass = []; // non-functional
+    
+    for(var i=0;i<param.questions.length;i++){
+        this.questions.push( new Question( param.questions[i] ) );
+    }
 }
 
-this.prototype.print = function(target){
-	var content = '';
-	content += '<div class="row-fluid" id="group_row_' + this.label + '">';
-		content += '<div class="' + this.cssClass.join(' ') + '"  id="group_content_' + this.label + '"> ';
+QuestionGroup.prototype.printTo = function(target){
+    target.append(this.print());
+}
 
-		
+QuestionGroup.prototype.print = function(){
+    var content = '';
+	content += '<div class="row-fluid" id="group_row_' + this.label + '">';
+    content += '<div class="' + this.cssClass + '"  id="group_content_' + this.label + '"> ';
+
+    $.each(this.questions,function(){
+        content += this.print();
+    });
+    
+    content += '</div>';
+    content += '</div>';
+    return content;
 }
